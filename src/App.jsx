83976cb413c11bcb03ffb00f56bee598d0aad733
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-10-29 22:19:01 +0100

    Definition of a component and returning JSX
    Here, I used a functional component, and the JSX renders an empty field.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import {Home} from './component jsx/main_menu/Home.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 
 function App() {
-    const [count, setCount] = useState(0)
+
 }
 
 export default App
