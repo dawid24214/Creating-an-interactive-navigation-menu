2c65c6c0d33733abf3427b4f7ff5c22a566389b1
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-10-29 22:53:26 +0100

    Installing the reactDom
    The ReactDOM library was needed for rendering the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import ReactDom from 'react-dom/client.js'
+import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
 import {Home} from './component jsx/main_menu/Home.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -7,5 +9,6 @@ import viteLogo from '/vite.svg'
 function App() {
 
 }
-
+const rootElement = document.getElementById('root');
+const root = ReactDom.createRoot()
 export default App
