2b04fe85ea5aa09898649218e523249fed680713
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-10-29 23:12:14 +0100

    Managing the application
    Thanks to the HomeLayout  component , I'm able to recognize the application more easily because the outlet is the parent for its other children .This way , the strucutre will be organized .

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import { useState } from 'react'
 import ReactDom from 'react-dom/client.js'
 import {BrowserRouter, Outlet, Route, Routes} from "react-router-dom";
 import {Home} from './component jsx/main_menu/Home.jsx'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+const HomeLayout = () => {
+    return(
+        < div className= 'App'>
+            <Home/>
+            <Outlet/>
+        </div>
+    )
+}
+
 
 
 function App() {
