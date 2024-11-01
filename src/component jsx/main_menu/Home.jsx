2a20b48efef5bf268b35a75b5cb4afcf60e900bb
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-11-01 12:59:12 +0100

    Creating navigation to the main menu
    At this point , I'm creating tiles for reloading the page

--- src/component jsx/main_menu/Home.jsx
+++ src/component jsx/main_menu/Home.jsx
@@ -2,6 +2,21 @@ import React from "react";
 
 export const Home = () => {
     return(
-        <></>
+        <>
+            <header className='header__container'>
+                <section className='header__registraion container'>
+                    <a href=''>Zaloguj się</a><br/>
+                    <a href=''>Rejestracja</a>
+                </section>
+                <section className='header__navigation'>
+                    <a href=''>Główne menu</a>
+                    <a href=''>Postacie</a>
+                    <a href=''>Ksiega potworów</a>
+                    <a href=''>umiejętności</a>
+                    <a href=''>Potwory</a>
+                </section>
+
+            </header>
+        </>
     )
 }
