9b75bf97d99896d9fc218da985897fe536c7b1f1
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2024-11-01 14:21:42 +0100

    adding subpages

--- src/component jsx/main_menu/Home.jsx
+++ src/component jsx/main_menu/Home.jsx
@@ -9,11 +9,10 @@ export const Home = () => {
                     <a href=''>Rejestracja</a>
                 </section>
                 <section className='header__navigation'>
-                    <a href=''>Główne menu</a>
-                    <a href=''>Postacie</a>
-                    <a href=''>Ksiega potworów</a>
-                    <a href=''>umiejętności</a>
-                    <a href=''>Potwory</a>
+                    <a href=''>Główne menu</a><br/>
+                    <a href=''>Postacie</a><br/>
+                    <a href=''>Ksiega potworów</a><br/>
+                    <a href=''>Umiejętności</a><br/>
                 </section>
 
             </header>
